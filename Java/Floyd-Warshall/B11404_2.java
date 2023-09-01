7acdfe6122c6db7da404d3a7d81b08aec56c488b
developerYHLee <dbsgh812@naver.com>
2023-09-01 16:11:27 +0900

    옮기기

---
+++ Java/Floyd-Warshall/B11404_2.java
@@ -0,0 +1,55 @@
+package Practice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B11404_2 { //플로이드
+	static int[][] map;
+	static final int INF = Integer.MAX_VALUE >> 1;
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		map = new int[N + 1][N + 1];
+		for(int[] arr : map) Arrays.fill(arr, INF);
+
+		for(int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			
+			//시작 도시에서 도착 도시까지 한번에 가는 값 저장
+			if(map[start][end] > cost) map[start][end] = cost;
+		}
+		
+		//v : 환승 지점, s : 출발 지점, e : 도착 지점
+		for(int v = 1; v <= N; v++) {
+			for(int s = 1; s <= N; s++) {
+				if(s == v) continue;
+				
+				for(int e = 1; e <= N; e++) {
+					if(s == e || e == v) continue;
+					
+					//저장되어 있던 가장 최소값과 탐색하고 있는 값 중 작은 값 저장
+					map[s][e] = Math.min(map[s][e], map[s][v] + map[v][e]);
+				}
+			}
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) sb.append(map[i][j] == INF ? 0 : map[i][j]).append(" ");
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
