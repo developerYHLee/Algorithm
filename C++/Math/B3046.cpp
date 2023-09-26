de984a0f900a1266d486b19c6811c4c4da32dccb
developerYHLee <dbsgh812@naver.com>
2023-09-26 12:44:40 +0900

    R2

---
+++ C++/Math/B3046.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { //R2
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int r1, s;
+	cin >> r1 >> s;
+
+	cout << 2 * s - r1;
+}
