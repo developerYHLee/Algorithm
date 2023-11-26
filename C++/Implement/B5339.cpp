83238faf132d25967975acbebb2fcb7cd4741099
developerYHLee <dbsgh812@naver.com>
2023-11-26 23:35:31 +0900

    콜센터

---
+++ C++/Implement/B5339.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { //ÄÝ¼¾ÅÍ
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cout << "     /~\\\n"
+		<< "    ( oo|\n"
+		<< "    _\\=/_\n"
+		<< "   /  _  \\\n"
+		<< "  //|/.\\|\\\\\n"
+		<< " ||  \\ /  ||\n"
+		<< "============\n"
+		<< "|          |\n"
+		<< "|          |\n"
+		<< "|          |";
+}
