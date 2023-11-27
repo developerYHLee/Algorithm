6cdd88a77936d6f3148404bf0702968de42d87f5
developerYHLee <dbsgh812@naver.com>
2023-11-28 03:17:03 +0900

    마이크로소프트 로고

---
+++ C++/Implement/B5338.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { //마이크로소프트 로고
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cout << "       _.-;;-._\n"
+		<< "\'-..-\'|   ||   |\n"
+		<< "\'-..-\'|_.-;;-._|\n"
+		<< "\'-..-\'|   ||   |\n"
+		<< "\'-..-\'|_.-\'\'-._|";
+}
