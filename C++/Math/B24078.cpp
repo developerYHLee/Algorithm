7125c47543a783124d2d5a28bd6b188c05c441f9
developerYHLee <dbsgh812@naver.com>
2024-03-16 23:48:36 +0900

    余り (Remainder)

---
+++ C++/Math/B24078.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { //εωͺκ (Remainder)
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int X;
+	cin >> X;
+
+	cout << (X % 21);
+}
