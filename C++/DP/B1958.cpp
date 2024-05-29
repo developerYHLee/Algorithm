a7efa215338333a2d85f9797209df0ed6760fb01
developerYHLee <dbsgh812@naver.com>
2024-05-29 21:07:22 +0900

    LCS 3

--- C++/DP/B1958.cpp
+++ C++/DP/B1958.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,7 +18,7 @@ void solve() {
 		for (int j = 1; j < S2.size(); j++) {
 			for (int k = 1; k < S3.size(); k++) {
 				if (S1[i] == S2[j] && S1[i] == S3[k]) dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
-				else dp[i][j][k] = max(dp[i - 1][j][k], max(dp[i][j - 1][k], dp[i][j][k - 1]));
+				else dp[i][j][k] = max({ dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1] });
 			}
 		}
 	}
