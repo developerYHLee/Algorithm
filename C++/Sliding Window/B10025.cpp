8e7cfefa5bcc51c2882c472c46cb308e8efd960e
developerYHLee <dbsgh812@naver.com>
2024-06-03 19:15:12 +0900

    게으른 백곰

---
+++ C++/Sliding Window/B10025.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int N, K, G[2'000'001];
+
+void input() {
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) {
+		int g, x;
+		cin >> g >> x;
+
+		G[x] = g;
+	}
+	K = K * 2 + 1;
+}
+
+void solve() {
+	int l = 0, r = 0, res = 0, temp = 0;
+	while(r <= 1'000'000) {
+		if (r - l >= K) {
+			temp -= G[l++];
+		}
+		temp += G[r++];
+		res = max(res, temp);
+	}
+	cout << res;
+}
+
+int main() { //°ÔÀ¸¸¥ ¹é°õ
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	input();
+	solve();
+}
