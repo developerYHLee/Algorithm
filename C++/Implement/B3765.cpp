66c02382881aeb717e5ef811bd6714937b6d5e59
developerYHLee <dbsgh812@naver.com>
2024-07-23 23:26:37 +0900

    Celebrity jeopardy

---
+++ C++/Implement/B3765.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { //Celebrity jeopardy
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string str;
+
+	while (cin >> str) cout << str << "\n";
+}
