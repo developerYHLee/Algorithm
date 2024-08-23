5dc1112c6ec36eddc8df1e74bc7819c87771eb7d
developerYHLee <dbsgh812@naver.com>
2024-08-23 17:02:04 +0900

    줄번호

---
+++ C++/Implement/B4470.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() { //ÁÙ¹øÈ£
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+	cin.ignore();
+
+	for (int i = 1; i <= N; i++) {
+		string str;
+		getline(cin, str);
+
+		cout << i << ". " << str << "\n";
+	}
+}
